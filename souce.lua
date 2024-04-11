aae47b7aa37229779bb63f303b36e104a9defcf4
Breadido <142183346+Breadido@users.noreply.github.com>
2024-04-11 12:23:05 +0700

    Update souce.lua

--- souce.lua
+++ souce.lua
@@ -1,7 +1,50 @@
-
 local uh = {}
 
 local firstTabOpened = false
+function uh:DraggingEnabled(frame, parent)
+	parent = parent or frame
+
+	local gui = parent
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+
+	local function update(input)
+		local delta = input.Position - dragStart
+		game:GetService("TweenService"):Create(gui,TweenInfo.new(0.15), {
+			Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		}):Play()
+		--gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+
+	game:GetService("UserInputService").InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
+end
+
 function uh.SetupUI(conpigs)
 	conpigs = conpigs
 	conpigs.Title = conpigs.Title or "beat your meat"
@@ -46,6 +89,7 @@ function uh.SetupUI(conpigs)
 	UITitle.TextSize = 15.000
 	UITitle.TextXAlignment = Enum.TextXAlignment.Left
 	UITitle.TextYAlignment = Enum.TextYAlignment.Bottom
+	uh:DraggingEnabled(UITitle, MainFrame)
 
 	TabList.Name = "TabList"
 	TabList.Parent = MainFrame
@@ -119,6 +163,10 @@ function uh.SetupUI(conpigs)
 		TabListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
 		TabListLayout_2.Padding = UDim.new(0, 2)
 		
+		game:GetService("RunService").RenderStepped:Connect(function()
+			Main_2.CanvasSize = UDim2.new(0,0,0,TabListLayout.AbsoluteContentSize.Y)
+		end)
+		
 		local tabc = ContainerFrame
 		local tabl = TabList
 		for i, v in next, tabl:GetChildren() do
@@ -136,6 +184,8 @@ function uh.SetupUI(conpigs)
 				end)
 			end
 		end
+		
+		
 		
 		local nuhuh = {}
 		function nuhuh.CreateToggle(conpig)
